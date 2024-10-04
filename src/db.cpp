4b59d8f13ee71321b106e8c702d90841d94ad821
StrBoll <phillipboll107@gmail.com>
2024-10-04 09:32:26 -0400

    insertImageDB function

--- src/db.cpp
+++ src/db.cpp
@@ -9,8 +9,21 @@ using namespace std;
 
 bool insertImageDB(const char *image_binary, const char* file_name){
     try {
+        connection C("dbname=submits_database user=your_local_user password='your_local_password' host=localhost port=5432");
+
+        if (C.is_open()){
+
+            work W(C);
+
+            string command = "INSERT INTO images_submitted (image_data, file_name) VALUES " + image_binary + ", " + file_name + ";";
+
+            W.exec(command);
+            W.commit();
+
+            return true;
 
 
+        }
     }
     catch (const exception &e){
         cerr << e.what() << endl;
@@ -22,6 +35,8 @@ bool insertImageDB(const char *image_binary, const char* file_name){
 
 bool insertUserDB(int ufid, string first_name, string last_name){
     try {
+        connection C("dbname=submits_database user=your_local_user password='your_local_password' host=localhost port=5432");
+
 
 
     }
