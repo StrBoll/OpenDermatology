66c1a8e25d015e164982337e93ff0a6919f337ee
StrBoll <phillipboll107@gmail.com>
2024-10-06 18:40:29 -0400

    logins database

---
+++ src/connect/loginAuth.cpp
@@ -0,0 +1,36 @@
+#include <pqxx/pqxx>
+#include <iostream>
+
+
+using namespace pqxx;
+using namespace std;
+
+bool userExists(string username){
+
+    try {
+        connection C("dbname=logins user=phillipboll3 password='NewPassword2024' host=localhost port=5432");
+
+
+        if (C.is_open()){
+
+            nontransaction N(C);
+
+            string command = "SELECT * FROM LOGINS WHERE username_ = " + username + ";";
+
+            result results(N.exec(command));
+
+            if (results.empty()){
+                cout << "user could not be found" << endl;
+                return false;
+            } else {
+                return true;
+            }
+        }
+        
+
+    } catch (const exception &e){
+        cerr << e.what() << endl;
+    }
+
+
+}
