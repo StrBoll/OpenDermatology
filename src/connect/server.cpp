0b5857a6e7ac4817d1e0fffab73705578e9cb142
StrBoll <phillipboll107@gmail.com>
2024-10-07 20:57:52 -0400

    upload image clause

--- src/connect/server.cpp
+++ src/connect/server.cpp
@@ -6,7 +6,7 @@ using namespace std;
 using namespace pqxx;
 
 
-int mian () {
+int main () {
     // how to create functions for http requests: 
 
     /*app().registerHandler("/NameofFunction", [](const HttpRequest &request, function<void(const HttpResponse &)> &&callback){
@@ -15,9 +15,23 @@ int mian () {
 
         you can even implement pqxx to connect to the database in these http request called functions
 
-        
+
     });
     */
+
+
+   app().registerHandler("/uploadImage", [](const HttpRequest &request, function<void(const HttpResponse&)> &&callback){
+
+
+    // for handling uploading an image to the frontend
+
+    // should send the image for processing through /cpp/input.cpp
+
+
+    // afterwards, should connect to images database and store the image binary data there 
+
+    
+   });
 
 
 }
