29c4ddd799a9d71db1dff48a8310128cca33c7f7
StrBoll <phillipboll107@gmail.com>
2024-10-07 20:55:18 -0400

    how to make an http request function on a restapi

--- src/connect/server.cpp
+++ src/connect/server.cpp
@@ -7,7 +7,17 @@ using namespace pqxx;
 
 
 int mian () {
+    // how to create functions for http requests: 
+
+    /*app().registerHandler("/NameofFunction", [](const HttpRequest &request, function<void(const HttpResponse &)> &&callback){
+
+        in here you would write your code for what it does etc..
+
+        you can even implement pqxx to connect to the database in these http request called functions
+
+        
+    });
+    */
 
 
-    
 }
