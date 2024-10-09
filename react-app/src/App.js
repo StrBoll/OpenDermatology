c76c64064f7f7fb34147630dec7c8fdc45203ead
StrBoll <phillipboll107@gmail.com>
2024-10-08 21:54:06 -0400

    image to input.cpp function

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,26 @@ import './styles/App.css';
 import LoginPage from './pages/login';
 import Image from "./components/Uploadimage";
 
+
+const imageToFolder = async (upload) => {
+  const data = new FormData();
+  data.append("skin_image", upload);
+
+  try {
+    const response = await fetch('http://localhost:18080/uploadImage', {
+      method: 'POST',
+      body: data
+    });
+
+    if (response.ok){
+      console.log("Image upload good")
+    }
+
+  } catch (error) {
+    console.error("Issue with image function: ", error);
+  }
+};
+
 function App() {
 return (
   <Router>
