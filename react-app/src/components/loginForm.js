6111c0dc058b3c67c08f9610f64c2d9f4db80f50
Justin <roblujm@gmail.com>
2024-10-11 13:45:14 -0400

    email sign in with ufl email

--- react-app/src/components/loginForm.js
+++ react-app/src/components/loginForm.js
@@ -1,44 +1,43 @@
-// src/components/LoginForm.js
 import { auth, googleprovider } from '../config/firebase-config';
-import { signInWithPopup, signOut} from 'firebase/auth'
+import { signInWithPopup, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 
 export const LoginForm = ({ onLogin }) => {
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
 
-
-const signIn = async () => {
-  try{
-    await signInWithPopup(auth, googleprovider);
-  } catch (err){
-    console.error(err);
+  
+  const user = auth.currentUser;
+  if (user) {
+    console.log(user.email);
+  } else {
+    console.log("No user is logged in");
   }
- 
 
-};
+  const signIn = async () => {
+    try {
+      await signInWithPopup(auth, googleprovider);
+      
+      if (onLogin) {
+        onLogin(auth.currentUser);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-const signout = async () => {
-  try{
-    await signOut(auth);
-  } catch (err){
-    console.error(err);
-  }
- 
-
-};
-
+  const signout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div>
-      <input placeholder='Email...'
-      onChange={(e) => setEmail(e.target.value)}/>
-      <input placeholder='Password...'
-      onChange={(e) => setPassword(e.target.value)}/>
-      <button onClick={signIn}>Sign in</button>
+    
+      <button onClick={signIn}>Sign in with ufl email</button>
       <button onClick={signout}>Logout</button>
-
-</div>
+    </div>
   );
 };
 
