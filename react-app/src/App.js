6aed547da0135114d79f36c23391cb4d153a2b62
Justin <roblujm@gmail.com>
2024-10-11 17:38:34 -0400

    sign in authentication fixed

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './styles/App.css';
 import LoginPage from './pages/login';
+import LoginForm from "./components/loginForm";
 import Image from "./components/Uploadimage";
 
 
