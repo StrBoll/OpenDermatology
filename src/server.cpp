1346345e53af4ea4ec56baf7420e720e030b7468
StrBoll <phillipboll107@gmail.com>
2024-10-13 19:09:17 -0400

    CORS allows successful image upload message with only healthcheck

--- src/server.cpp
+++ src/server.cpp
@@ -4,29 +4,84 @@
 #include <drogon/HttpRequest.h>
 #include <pqxx/pqxx>
 #include "input.h"
+#include <opencv2/opencv.hpp>
 
 using namespace drogon;
 using namespace std;
 using namespace pqxx;
+using namespace cv;
+
+
+
+
 
 int main () {
-   // Health check endpoint
    app().registerHandler("/healthCheck", [](const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
         auto resp = HttpResponse::newHttpResponse();
+        resp->addHeader("Access-Control-Allow-Origin", "*");  // Allow all origins
+        resp->addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        resp->addHeader("Access-Control-Allow-Headers", "Content-Type");
         resp->setBody("Server is running");
         callback(resp);
     });
 
-   // Upload image endpoint
-   app().registerHandler("/uploadImage", [](const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+
+
+
+   
+app().registerHandler("/uploadImage", [](const HttpRequestPtr& req, std::function<void(const HttpResponsePtr &)> &&callback) {
+        try {
+        MultiPartParser fileUpload;
         
+        if (fileUpload.parse(req) != 0 || fileUpload.getFiles().empty()) {
+            throw runtime_error("No files uploaded or parsing failed");
+        }
+
+
+        for (const auto& file : fileUpload.getFiles()) {
+            LOG_INFO << "Received file: " << file.getFileName() 
+             << " with key: " << file.getItemName();
+}
+
+
+        auto &file = fileUpload.getFiles()[0];
+
+        auto byteStream = file.fileContent();
+
+        vector<uchar> buffer(byteStream.begin(), byteStream.end());
+        Mat img = imdecode(buffer, IMREAD_COLOR);
+
+        if (img.empty()){
+            throw runtime_error("Image is empty");
+        }
+
+        if (processImage(img) == false){
+            throw runtime_error("Image went to processing but returned failure");
+        }
 
         auto resp = HttpResponse::newHttpResponse();
-        resp->setBody("No file uploaded or wrong request method.");
+        resp->setBody("Image processed !!!!!");
         callback(resp);
-    });
 
-    // Set the server to listen on port 3000
+
+        } catch (const exception & e){
+            cerr << e.what() << endl;
+            auto resp = HttpResponse::newHttpResponse();
+            resp->setStatusCode(k500InternalServerError);
+            resp->setBody("Try block unable to execute");
+            callback(resp);
+        }
+
+        });
+
+
+        
+
+
+
+
+
+
     app().setLogLevel(trantor::Logger::kTrace);  
     LOG_INFO << "Starting server on port 3000";
     app().addListener("0.0.0.0", 3000).run();
