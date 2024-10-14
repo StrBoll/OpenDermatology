fd990171cb51bfcdd67125fe2a1edc6ddffcf801
StrBoll <phillipboll107@gmail.com>
2024-10-13 21:29:29 -0400

    pulled changes for login form

--- react-app/src/components/loginForm.js
+++ react-app/src/components/loginForm.js
@@ -1,9 +1,12 @@
 import { auth, googleprovider } from '../config/firebase-config';
 import { signInWithPopup, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginForm = ({ onLogin }) => {
- 
+
+  const navigate = useNavigate();
   const user = auth.currentUser;
   if (user) {
     console.log(user.email);
@@ -23,6 +26,7 @@ export const LoginForm = ({ onLogin }) => {
     try {
       if (email.endsWith("@ufl.edu")) {
         console.log("success");
+        navigate("/openderm");
       }
       else {
         alert('Invalid email or password');
@@ -35,6 +39,7 @@ export const LoginForm = ({ onLogin }) => {
   const signout = async () => {
     try {
       await signOut(auth);
+      console.log("signed out");
     } catch (err) {
       console.error(err);
     }
